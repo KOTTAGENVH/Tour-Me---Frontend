a7037ae0e1c9345585e535c41501278348b29817
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2024-02-08 15:57:26 +0530

    Update view-all-hotels.jsx

--- frontend/src/Pages/Customer/Hotel/view-all-hotels.jsx
+++ frontend/src/Pages/Customer/Hotel/view-all-hotels.jsx
@@ -9,10 +9,15 @@ import CircularProgress from "@mui/material/CircularProgress";
 import { getallhotels } from "../../../Api/services/hotelService";
 import Viewallcard from "../../../Components/viewallcard";
 import StickyFooter from "../../../Components/StickyFooter";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 function Hotels() {
   const [progress, setProgress] = React.useState(0);
   const [searchQuery, setSearchQuery] = useState("");
+  const [category, setCategory] = useState("");
   const [page, setPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(10);
 
@@ -63,91 +68,147 @@ function Hotels() {
     setPage(newPage);
   };
 
+  const filteredHotels = data?.filter((hotels) => {
+    // Check if the hotel's category matches the selected category or if no category is selected
+    const categoryMatch = category === "" || hotels.category === category;
+  
+    // Apply other filters only if the category matches
+    if (categoryMatch) {
+      return (
+        hotels.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        hotels.maindescription.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        hotels.price.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        hotels.Address.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        hotels.Address1.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    } else {
+      // If the category doesn't match, exclude the hotel
+      return false;
+    }
+  });
 
-  const filteredHotels = data?.filter(hotels =>
-    hotels.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
-    hotels.maindescription.toLowerCase().includes(searchQuery.toLowerCase()) ||
-    hotels.price.toLowerCase().includes(searchQuery.toLowerCase()) ||
-    hotels.Address.toLowerCase().includes(searchQuery.toLowerCase()) ||
-    hotels.Address1.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
   return (
     <div
       style={{
-        height: "120vh",
+        height: isMobile ? "140vh" : "100vh",
       }}
     >
       <Header />
-      <TextField
-        id="search-hotels"
-        label="Search Hotels"
-        variant="standard"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        sx={{
-          color: handleDarkmode(),
-          width: "70%",
-          margin: "auto",
-          marginTop: "20px",
-          marginBottom: "20px",
+      <div
+        style={{
           display: "flex",
-          justifySelf: "center",
-          alignContent: "center",
-          alignSelf: "center",
-          justifyContent: "center",
-          padding: "5px",
-          justifyItems: "center",
-          backgroundColor: "rgba(255, 255, 255, 0.1)",
-          backdropFilter: "blur(10px)",
-          borderRadius: "20px",
-          boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
-          "& .MuiInputAdornment-positionEnd": {
-            marginLeft: "0px",
-            marginRight: "0px",
-            color: "white",
-            "&:hover": {
-              cursor: "pointer",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+        }}
+      >
+  <FormControl
+    sx={{
+      backgroundColor: "rgba(255, 255, 255, 0.1)",
+      borderRadius: "20px",
+    }}
+  >
+    <InputLabel
+      id="demo-simple-select-label"
+      sx={{
+        color: handleDarkmode(),
+      }}
+    >
+      Category Filter
+    </InputLabel>
+    <Select
+      labelId="demo-simple-select-label"
+      id="demo-simple-select-helper"
+      value={category}
+      label="Category"
+      onChange={(e) => setCategory(e.target.value)}
+      sx={{
+        backgroundColor: "rgba(255, 255, 255, 0.1)",
+        width: "120px",
+        borderRadius: "20px",
+        color: handleDarkmode(),
+      }}
+    >
+      <MenuItem value={""}> None </MenuItem>
+      {data!== undefined && !isError && !isLoading && data!==0 && data?.map((destination) => (
+        <MenuItem key={destination.category} value={destination.category}>
+          {destination.category}
+        </MenuItem>
+      ))}
+    </Select>
+  </FormControl>
+
+        <TextField
+          id="search-hotels"
+          label="Search Hotels"
+          variant="standard"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          sx={{
+            color: handleDarkmode(),
+            width: "50%",
+            // marginLeft: "20px",
+            marginTop: "20px",
+            marginBottom: "20px",
+            display: "flex",
+            justifySelf: "center",
+            alignContent: "center",
+            alignSelf: "center",
+            justifyContent: "center",
+            padding: "5px",
+            justifyItems: "center",
+            backgroundColor: "rgba(255, 255, 255, 0.1)",
+            backdropFilter: "blur(10px)",
+            borderRadius: "20px",
+            boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
+            "& .MuiInputAdornment-positionEnd": {
+              marginLeft: "0px",
+              marginRight: "0px",
               color: "white",
-              transition: "all 0.3s ease",
-              transform: "scale(1.1)",
-              "&:active": {
-                transform: "scale(1.2)",
-                transition: "all 0.1s ease",
+              "&:hover": {
+                cursor: "pointer",
+                color: "white",
+                transition: "all 0.3s ease",
+                transform: "scale(1.1)",
+                "&:active": {
+                  transform: "scale(1.2)",
+                  transition: "all 0.1s ease",
+                },
               },
             },
-          },
-        }}
-        InputProps={{
-          border: "none",
-          disableUnderline: true,
-          endAdornment: (
-            <SearchIcon
-              sx={{
-                color: handleDarkmode(),
-                justifyContent: "center",
-                display: "flex",
-                "&:hover": {
-                  cursor: "pointer",
-                  transition: "all 0.3s ease",
-                  transform: "scale(1.1)",
-                  "&:active": {
-                    transform: "scale(1.2)",
-                    transition: "all 0.1s ease",
+          }}
+          InputProps={{
+            border: "none",
+            disableUnderline: true,
+            endAdornment: (
+              <SearchIcon
+                sx={{
+                  color: handleDarkmode(),
+                  justifyContent: "center",
+                  display: "flex",
+                  "&:hover": {
+                    cursor: "pointer",
+                    transition: "all 0.3s ease",
+                    transform: "scale(1.1)",
+                    "&:active": {
+                      transform: "scale(1.2)",
+                      transition: "all 0.1s ease",
+                    },
                   },
-                },
-              }}
-            />
-          ),
-          inputProps: {
-            style: {
-              fontSize: "2vh",
-              color: handleDarkmode(),
+                }}
+              />
+            ),
+            inputProps: {
+              style: {
+                fontSize: "2vh",
+                color: handleDarkmode(),
+              },
             },
-          },
-        }}
-      />
-
+          }}
+        />
+      </div>
       {isLoading && (
         <div
           style={{
@@ -170,18 +231,22 @@ function Hotels() {
           justifyContent: "center",
         }}
       >
-      {filteredHotels!==undefined && !isLoading && !isError && data?.length!==0 && filteredHotels
-          ?.slice((page - 1) * itemsPerPage, page * itemsPerPage)
-          ?.map((destination) => (
-            <Viewallcard
-              key={destination._id}
-              id={destination._id}
-              title={destination.title}
-              description={destination.maindescription}
-              image={destination.image}
-              price={destination.price}
-            />
-          ))}
+        {filteredHotels !== undefined &&
+          !isLoading &&
+          !isError &&
+          data?.length !== 0 &&
+          filteredHotels
+            ?.slice((page - 1) * itemsPerPage, page * itemsPerPage)
+            ?.map((destination) => (
+              <Viewallcard
+                key={destination._id}
+                id={destination._id}
+                title={destination.title}
+                description={destination.maindescription}
+                image={destination.image}
+                price={destination.price}
+              />
+            ))}
       </div>
       <div>
         {Array.from({ length: totalPages }).map((_, index) => (
